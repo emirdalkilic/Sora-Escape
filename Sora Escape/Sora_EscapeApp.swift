1c0971733938664d0280fe93b2ebf003ed0418d3
Emir Dalkılıç <c.emirdalkilic@gmail.com>
2026-04-06 15:49:23 +0300

    Initial Commit

---
+++ Sora Escape/Sora_EscapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sora_EscapeApp.swift
+//  Sora Escape
+//
+//  Created by Emir Dalkılıç on 6.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct Sora_EscapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
